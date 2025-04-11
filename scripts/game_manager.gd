1a698284bbb24c1b48c68ddfd7125c361925e3db
Seal <88384688+Seal187@users.noreply.github.com>
2025-04-11 02:44:33 +0200

    Boilerplates and Placeholders
    (High-Tier Shitpost)

---
+++ scripts/game_manager.gd
@@ -0,0 +1,10 @@
+extends Node
+
+func _ready():
+	process_mode = Node.PROCESS_MODE_ALWAYS
+		
+func _process(_delta):
+	if Input.is_action_just_pressed("Escape"):
+		$"../PauseMenu".visible = not $"../PauseMenu".visible
+		get_tree().paused = not get_tree().paused
+		$"../PauseMenu".resetUI()
