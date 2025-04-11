1d1e6ed115aa1af6d70c0f9d05932bb0ca697f31
towurzer <t.wurzer.2004@gmail.com>
2025-04-11 21:32:25 +0200

    added position check

--- scripts/globals.gd
+++ scripts/globals.gd
@@ -8,3 +8,13 @@ enum movement {
 	LEFT, 
 	HIDE 
 	}
+
+var starting_pos: Vector2i = Vector2i(0, 0)
+
+var valid_pos: Array[Vector2i] = [
+	Vector2i(0, 0), Vector2i(0, 1), Vector2i(0, 2), Vector2i(0, 3), Vector2i(0, 4),
+	Vector2i(1, 0), Vector2i(1, 1), Vector2i(1, 2), Vector2i(1, 3), Vector2i(1, 4),
+	Vector2i(2, 0), Vector2i(2, 1), Vector2i(2, 2), Vector2i(2, 3), Vector2i(2, 4),
+	Vector2i(3, 0), Vector2i(3, 1), Vector2i(3, 2), Vector2i(3, 3), Vector2i(3, 4),
+	Vector2i(4, 0), Vector2i(4, 1), Vector2i(4, 2), Vector2i(4, 3), Vector2i(4, 4)
+]
--- scripts/planner.gd
+++ scripts/planner.gd
@@ -3,31 +3,37 @@ extends Control
 signal heist_planned(sequence) # signal when done
 
 var input_sequence: Array[Globals.movement] = []
-var game_scene: String = ""
+var valid_pos: Array[Vector2i] = Globals.valid_pos.duplicate(true)
+var starting_pos: Vector2i = Globals.starting_pos
+var current_pos: Vector2i = starting_pos
 
 @onready
 var sequence_display_label: Label = $VBoxContainer/Sequence
 @onready
 var instructions_label: Label = $VBoxContainer/InstructionLabel
+@onready
+var error_label: Label = $VBoxContainer/NEIN
 
 #-----------------------------------------------------------------------
 
 func _ready() -> void:
 	# Update display in case we load the last sequence till failure, to avoid needing to start over again. 
-	update_display()
+	update_display(true)
 
 func _input(event: InputEvent) -> void:
 	if not event.is_pressed():
 		return
 
+	var move: Globals.movement = Globals.movement.HIDE
+	
 	if event.is_action("PlannerUp", true):
-		add_action(Globals.movement.UP)
+		move = Globals.movement.UP
 	elif event.is_action("PlannerDown", true):
-		add_action(Globals.movement.DOWN)
+		move = Globals.movement.DOWN
 	elif event.is_action("PlannerLeft", true):
-		add_action(Globals.movement.LEFT)
+		move = Globals.movement.LEFT
 	elif event.is_action("PlannerRight", true):
-		add_action(Globals.movement.RIGHT)
+		move = Globals.movement.RIGHT
 	elif event.is_action("PlannerHide", true):
 		add_action(Globals.movement.HIDE)
 	elif event.is_action("PlannerDelete", true):
@@ -35,25 +41,41 @@ func _input(event: InputEvent) -> void:
 	elif event.is_action("PlannerCommit", true):
 		finalize_sequence()
 
+
+	if move != Globals.movement.HIDE:
+		if (check_move(move)):
+			add_action(move)
+		else:
+			# TODO: Signal user invalid move by shaking screen
+			update_display(false)
+			pass
+		
+		
 func add_action(action_string: Globals.movement) -> void:
 	input_sequence.append(action_string)
-	update_display()
+	update_display(true)
 
 func remove_last_action() -> void:
 	if not input_sequence.is_empty():
 		input_sequence.pop_back() 
-		update_display()
+		update_display(true)
 
-func update_display() -> void:
+func update_display(valid: bool) -> void:
 	if input_sequence.is_empty():
 		sequence_display_label.text = "[Empty Sequence]"
 	else:
 		sequence_display_label.text = " -> ".join(input_sequence)
+	
+	if valid:
+		error_label.text = ":)"
+	else: 
+		error_label.text = "Bist du dumm?"
+		# TODO signal the user, shaking label
 
 func finalize_sequence() -> void:
 	if input_sequence.is_empty():
 		print("Cannot finalize an empty sequence.")
-		# TODO signal the user, shaking label
+		update_display(false)
 		return
 
 	print("Sequence finalized: ", input_sequence)
@@ -61,8 +83,29 @@ func finalize_sequence() -> void:
 
 func clear_sequence() -> void:
 	input_sequence.clear()
-	update_display()
+	update_display(true)
 
 func load_sequence(sequence: Array) -> void:
 	input_sequence = sequence.duplicate() 
-	update_display()
+	update_display(true)
+
+func check_move(move: Globals.movement) -> bool:
+	var new_position = current_pos
+	
+	if (move == Globals.movement.UP):
+		new_position[0] += 1
+	elif (move == Globals.movement.DOWN):
+		new_position[0] -= 1
+	elif (move == Globals.movement.RIGHT):
+		new_position[1] += 1
+	elif (move == Globals.movement.LEFT):
+		new_position[1] -= 1
+	else:
+		push_error("MICHI is fett am cheaten")
+		
+	
+	if new_position in valid_pos:
+		current_pos = new_position
+		return true
+	
+	return false
